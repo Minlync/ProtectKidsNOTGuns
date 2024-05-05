63ee65e5fe849dd0599a005a6cb6caaacd49eca7
Minlync <93282772+Minlync@users.noreply.github.com>
2024-05-05 19:53:45 -0400

    update data

--- sketch.js
+++ sketch.js
@@ -33,18 +33,7 @@ function draw() {
     }
 }
 
-function mousePressed() {
-    if (currentScene instanceof CoverOne) {
-        currentScene.mousePressed();  
-    } else if (currentScene instanceof PageTwo) {
-        currentScene = new PageThree();
-    } else if (currentScene instanceof PageThree){
-        currentScene = new PageFour(); 
-    } else if (currentScene instanceof PageFour){
-        currentScene = new PageFive(); 
-        currentScene.startAnimation = true; 
-    }
-}
+
 
 function spawnParticles() {
     for (let i = 0; i < img.width; i += RESOLUTION) {
@@ -192,18 +181,184 @@ class PageFour {
         this.texty = 50;
         this.datax = 120;
         this.rectColor = 255;
-        this.rectWidth1 = 1;
-        // Initialize other rectWidths
-        this.startAnimation = false;
+        this.rectWidth1 = 0;
+        this.rectWidth2 = 0;
+        this.rectWidth3 = 0;
+        this.rectWidth4 = 100;
+        this.rectWidth5 = 100;
+        this.rectWidth6 = 100;
+        this.rectWidth7 = 100;
+        this.rectWidth8 = 100;
+        this.rectWidth9 = 100;
+        this.rectWidth10 = 0;
+        this.rectWidth11 = 0;
+        this.rectWidth12 = 0;
+        this.rectHeight = 25;
+        this.animationSpeed = 1; 
+        this.startAnimation = true;
     }
 
     display() {
-        // Display data
+        
+        // here is the data of gunshoots in schools
+
+        fill (this.rectColor);
+        noStroke();
+        textSize(18);
+        text("textplaceholder", this.textx, 20,700);
+
+        fill(this.rectColor);
+        textSize(18);
+        text("2010-11",this.datax-80,100);
+        rect(this.datax, 80, this.rectWidth1, this.rectHeight);
+
+        textSize(18);
+        text("2011-12",this.datax-80,115+this.rectHeight);
+        rect(this.datax, 80+40, this.rectWidth2, this.rectHeight);
+    
+        fill(this.rectColor);
+        textSize(18);
+        text("2012-13",this.datax-80,155+this.rectHeight);
+        rect(this.datax, 80*2, this.rectWidth3, this.rectHeight);
+    
+        fill(this.rectColor);
+        textSize(18);
+        text("2013-14",this.datax-80,195+this.rectHeight);
+        rect(this.datax, 80*2+40, this.rectWidth4, this.rectHeight);
+    
+        //rect 5 data
+        textSize(18);
+        text("2014-15",this.datax-80,235+this.rectHeight);
+        rect(this.datax, 80*2+80, this.rectWidth5, this.rectHeight);
+    
+        //rect 6 data
+        textSize(18);
+        text("2015-16",this.datax-80,275+this.rectHeight);
+        rect(this.datax, 80*2+120, this.rectWidth6, this.rectHeight);
+    
+        //rect 7 data
+        textSize(18);
+        text("2016-17",this.datax-80,315+this.rectHeight);
+        rect(this.datax, 80*2+160, this.rectWidth7, this.rectHeight);
+         //rect 8 data
+  
+        textSize(18);
+        text("2017-18",this.datax-80,353+this.rectHeight);
+        rect(this.datax, 80*2+200, this.rectWidth8, this.rectHeight);
+    
+        //rect 9 data
+    
+        textSize(18);
+        text("2018-19",this.datax-80,393+this.rectHeight);
+        rect(this.datax, 80*2+200+40, this.rectWidth9, this.rectHeight);
+    
+        //rect 10 data
+    
+        textSize(18);
+        text("2019-20",this.datax-80,433+this.rectHeight);
+        rect(this.datax, 80*2+200+80, this.rectWidth10, this.rectHeight);
+    
+        //rect 11 data
+    
+        textSize(18);
+        text("2020-21",this.datax-80,473+this.rectHeight);
+        rect(this.datax, 80*2+200+80+40, this.rectWidth11, this.rectHeight);
+    
+        fill(255,0,232);
+        textSize(18);
+        text("2021-23",this.datax-80,510+this.rectHeight);
+        rect(this.datax, 80+200+80+160, this.rectWidth12, this.rectHeight);
+
+
+
+
+
     }
 
     update() {
         if (this.startAnimation) {
-            // Animate data visualization
+            // animate the rectangle width
+            if (this.rectWidth1 < 100) {
+                this.rectWidth1 += this.animationSpeed;
+            } else {
+                this.rectWidth1 = 105;
+            }
+    
+          
+          if (this.rectWidth2 <50) {
+            this.rectWidth2 += this.animationSpeed;
+          } else{
+            this.rectWidth2 = 55;
+          }
+          
+          
+          if (this.rectWidth3 <80){
+            this.rectWidth3 += this.animationSpeed;
+          }else{
+            this.rectWidth3 = 85;
+          }
+          
+          
+          if(this.rectWidth4 < 110){
+            this.rectWidth4 += this.animationSpeed;
+          } else {
+            this.rectWidth4 = 115;
+          }
+          
+          if (this.rectWidth5 <160){
+            this.rectWidth5 += this.animationSpeed;
+            
+          }else{
+            this.rectWidth5 = 165;
+          }
+          
+          if (this.rectWidth6 <177) {
+            this.rectWidth6 += this.animationSpeed;
+          }else{
+            this.rectWidth6 = 187;
+          }
+          
+          if (this.rectWidth7 <105) {
+            this.rectWidth7 += this.animationSpeed;
+          } else {
+            this.rectWidth7 = 110;
+          }
+          
+          if (this.rectWidth8 <140){
+            this.rectWidth8 += this.animationSpeed;
+          } else{
+            this.rectWidth = 145;
+          }
+          
+          
+        } if (this.rectWidth9 < 245) {
+          this.rectWidth9 += this.animationSpeed;
+        }else{
+          this.rectWidth = 255;
+        }
+          
+        if (this.rectWidth10 < 345) {
+          this.rectWidth10 += this.animationSpeed;
+        }else{
+          this.rectWidth10 =355;
+        }
+          
+          if (this.rectWidth11 < 385) {
+          this.rectWidth10 += this.animationSpeed;
+        }else{
+          this.rectWidth10 =390;
+        }
+          
+          if (this.rectWidth11 < 440) {
+          this.rectWidth11 += this.animationSpeed;
+        }else{
+          this.rectWidth11 =441;
+        }
+          
+         if (this.rectWidth12 < 555) {
+          this.rectWidth12 += this.animationSpeed;
+        }else{
+          this.rectWidth12 =556;
         }
     }
 }
@@ -223,3 +378,17 @@ class PageFive {
 
 // switch
 
+function mousePressed() {
+    if (currentScene instanceof CoverOne) {
+        currentScene.mousePressed();  
+    } else if (currentScene instanceof PageTwo) {
+        currentScene = new PageThree();
+    } else if (currentScene instanceof PageThree){
+        currentScene = new PageFour(); 
+       } else if (currentScene instanceof PageFour){
+        currentScene = new PageFive(); 
+      
+      currentScene.startAnimation = true; 
+  
+  }
+}
